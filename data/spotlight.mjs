bf3d192b3241cb8fdcb7ce4985aff1f400248b0b
Kelsey-Hansel <kelseylhansel@gmail.com>
2026-04-17 16:51:29 -0500

    Fixed image paths I missed.

--- data/spotlight.mjs
+++ data/spotlight.mjs
@@ -4,20 +4,20 @@ export const books = [
             "author": "Kenton Kilgore",
             "genre": "Thriller",
             "description": "She's lost and alone on a desolate world of monsters never before imagined. And if they don't kill her, the witch who has her boyfriend will. Alyx is a teenaged misfit who likes motorcycles, vintage hard rock, and her boyfriend Sam, a misfit in his own way. Alyx endures horrors and heartbreak as she learns that the witch is but a slave of the ancient, inhuman being who rules this wasted land—and who craves to take Alyx and Sam for himself.",
-            "coverImage": "images/this-wasted-land.jpg"
+            "coverImage": "./images/this-wasted-land.jpg"
         },
         {
             "title": "Whispers Most Foul",
             "author": "Emma MacDonald",
             "genre": "Dark Fantasy",
             "description": "Rose Thenlif, the only student unable to cast spells at the elite Dunhollow Academy of Witchcraft, prefers keeping her nose in books and away from the sneers of her classmates, or the critical eyes of her headmistress mother. But when her fellow classmates begin to go missing, she finds that she alone can see their ghosts.",
-            "coverImage": "images/whispers-most-foul.jpg"
+            "coverImage": "./images/whispers-most-foul.jpg"
         },
         {
             "title": "A Tale of Ashes",
             "author": "Ann Dayleview",
             "genre": "Fantasy",
             "description": "She's hunting the story of a lifetime. But she's not the only one stalking in the dark. A Tale of Ashes is a fairy tale mash up, set in a world where all the creatures from your childhood stories live alongside humans. But these creatures are not who you remember.",
-            "coverImage": "images/tale-of-ashes.jpg"
+            "coverImage": "./images/tale-of-ashes.jpg"
         }
     ]
